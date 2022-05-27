62db8f8ba22319425b3634c87454bfeeaf6d570b
Nguyen Phu Hai <nguyenphuhai89@gmail.com>
2022-05-28 01:14:48 +0700

    update sửa lỗi 'import {DISHES} from ./share-->/shared

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from "./components/MenuComponent";
-import{DISHES} from "./share/dishes";
+import{DISHES} from "./shared/dishes";
 
 class App extends Component {
   constructor(props){
