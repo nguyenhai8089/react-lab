1e44a1a99e88cd13442b92add29dea89b78ab2d1
Nguyen Phu Hai <nguyenphuhai89@gmail.com>
2022-05-28 00:05:29 +0700

    add danh sach mon an

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from "./components/MenuComponent"
 
 class App extends Component {
   render(){
@@ -15,6 +16,7 @@ class App extends Component {
             </NavbarBrand>
             </div>
         </Navbar>
+        <Menu/>
       </div>
     );
 
