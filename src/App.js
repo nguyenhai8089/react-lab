920618985430f1dbe6757c46112815587ca6272c
Nguyen Phu Hai <nguyenphuhai89@gmail.com>
2022-06-07 11:12:14 +0700

    check loi

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, {Component} from 'react';
 /* import logo from './logo.svg'; */
 import './App.css';
 import Main from "./components/MainComponent";
+import{BrowserRouter} from 'react-router-dom';
 
 
 
 class App extends Component { 
   render(){
     return (
-      <div className="App">        
-        <Main/>      
-      </div>
+      <BrowserRouter>
+         <div className="App">        
+            <Main/>      
+          </div>       
+      </BrowserRouter>
+      
     );
 
   }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,29 +2,34 @@ import React, {Component} from 'react';
 /* import logo from './logo.svg'; */
 /* import {Navbar, NavbarBrand} from 'reactstrap'; */
 import Menu from "./MenuComponent";
-import DishDetail from "./DishDetailComponent";
+
 import{DISHES} from "../shared/dishes";
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import{Switch,Route,Redirect} from 'react-router-dom';
 
 
-class Main extends Component {
+class Main extends Component {  
   constructor(props){
     super(props);
-    this.state={dishes: DISHES
-    , selectedDish:null}    
+    this.state={dishes: DISHES};    
     }
-    onDishSelect(dishId){
-        this.setState({selectedDish:dishId});
-
-  }
+  
   render(){
+    const HomePage=(props)=>{return(<Home/>);}
     
     return (
-      <div className="App">
+      <div>
         <Header/>
-        <Menu dishes={this.state.dishes}  onClick={(dishId)=>this.onDishSelect(dishId)}/>       
-        <DishDetail dish={this.state.dishes.filter((dish)=>dish.id===this.state.selectedDish)[0]}/>
+        <Switch>
+             <Route path="/home" componet={HomePage} />
+             <Route exact path="/menu" componet={()=><Menu dishes={this.state.dishes}/>} 
+             />
+             <Redirect to='/Home'/>
+
+        </Switch>
+        
         <Footer/> 
       </div>
     );
