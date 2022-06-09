357ea028aa55af257704b4bb1d77a58a87d8e709
Nguyen Phu Hai <nguyenphuhai89@gmail.com>
2022-06-09 10:27:00 +0700

    lab05_3

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,19 +8,34 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import{Switch,Route,Redirect} from 'react-router-dom';
+import Contact from './ContactComponent';
+import {PROMOTIONS} from '../shared/promotions';
+import {LEADERS} from '../shared/leaders';
+import {COMMENTS} from '../shared/comments';
 
 
 class Main extends Component {  
   constructor(props){
     super(props);
-    this.state={dishes: DISHES};    
+    this.state={
+      dishes: DISHES,
+      comments:COMMENTS,
+      promotions:PROMOTIONS,
+      leaders:LEADERS};    
     }
   
   render(){
-    const HomePage=(props)=>{
+    const HomePage=(props)=>{      
       return(
-      <Home/>
+      <Home 
+            dish ={this.state.dishes.filter((dish)=>dish.featured)[0]}
+            promotion={this.state.promotions.filter((promo)=>promo.featured)[0]}
+            leader={this.state.leaders.filter((leader)=>leader.featured)[0]}
+            
+      />
+      
       );
+      
     }
     
     return (
@@ -30,6 +45,7 @@ class Main extends Component {
              <Route path="/home" component={HomePage} />
              <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/>} 
              />
+             <Route exact path="/contactus" component={Contact}/>
              <Redirect to='/Home'/>
 
         </Switch>
