7a89499a4790452ab57e18fcd3d36e4aa4f5c699
Nguyen Phu Hai <nguyenphuhai89@gmail.com>
2022-06-14 14:28:05 +0700

    exercise 2

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,11 +1,96 @@
 import React from 'react';
-import { Card,CardBody,CardHeader,Media,Breadcrumb,BreadcrumbItem } from 'reactstrap';
+import { Card,CardBody,Media,Breadcrumb,BreadcrumbItem, CardHeader } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-const About=(props) =>{
-    return(
-        {}
-    );
+function About(props) {
+    const leaders =props.leaders.map((leader)=>{
+        return(
+            <div className='container'>
+                 <div className='row-container'>
+                    <img src={leader.image} alt={leader.name}/>
+                    <div className='div'>
+                         <h3 className='leader'>{leader.name}</h3>
+                         <p className="leader">{leader.designation}</p>
+                         <p className="leader">{leader.description}</p>                  
+
+                    </div>
+                 </div>
+            </div>
+            
+        );
+    });
+    return(         
+        <div className='container'>
+            <div className='row'>
+                <Breadcrumb>
+                    <BreadcrumbItem ><Link to="/home">home</Link></BreadcrumbItem>
+                     <BreadcrumbItem active>about us</BreadcrumbItem>
+                </Breadcrumb>
+                <div className='col-12'>
+                    <h3>About Us</h3>
+                    <hr/>
+
+                </div>
+            </div>
+            <div className='row row-content' >
+                 <div className='col-12 col-md-6'>
+                       <h2>Our History</h2>
+                       <p>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong. Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.</p>
+                       <p>The restaurant traces its humble beginnings <em>to The Frying Pan, a successful</em> chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</p>
+                 </div>
+                 <div className='col-12 col-md-6'>
+                     <Card>
+                        <CardHeader className='bg-primary text-white'>Facts At a Glance</CardHeader>
+                        <CardBody>
+                            <dl className='row p-1'>
+                                 <dt className="col-6">Started</dt>
+                                <dd className="col-6">3 Feb. 2013</dd>
+                                <dt className="col-6">Major Stake Holder</dt>
+                                <dd className="col-6">HK Fine Foods Inc.</dd>
+                                <dt className="col-6">Last Year's Turnover</dt>
+                                <dd className="col-6">$1,250,375</dd>
+                                <dt className="col-6">Employees</dt>
+                                <dd className="col-6">40</dd>
+                            </dl>
+
+                        </CardBody>
+                     </Card>
+
+                 </div>
+                 <div className='col-12'>
+                     <Card>
+                         <CardBody className='bg-faded'>
+                                <blockquote>
+                                     <p className='mb-0'>You better cut the pizza in four pieces because I'm not hungry enough to eat six.</p>
+                                     <footer className='blockquote-footer'>Yogi Berra, 
+                                         <cite title='Source Title'>The Wit and Wisdom of Yogi Berra, P. Pepe, Diversion Books, 2014 </cite>
+                                     </footer>
+                                </blockquote>
+                         </CardBody>
+                     </Card>
+
+                </div>
+            </div>
+            
+            <div className="row row-content">
+                <div className="col-12">
+                    <h2>Corporate Leadership</h2>
+                </div>
+                <div className="col-12">
+                    <div >
+                        <Media list>
+                            {leaders}
+                        </Media>
+                    </div>                   
+                </div>
+            </div>
+            
+        </div>
+    );    
+    
 }
+
+
+
 export default About;
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -51,7 +51,7 @@ class Main extends Component {
         <Switch>  
           <Route path='/home' component={HomePage} />
           <Route path='/menu/:dishId' component={DishWithId} />
-          <Route path='/about' component={About} />
+          <Route exact path='/aboutus' component={()=> <About leaders={this.state.leaders}/>} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
           <Route exact path='/contactus' component={Contact} />
           <Redirect to="/home" />
