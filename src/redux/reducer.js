80c393482d7c1184d31b9073cca6f814eb391e24
Nguyen Phu Hai <nguyenphuhai89@gmail.com>
2022-06-28 00:12:40 +0700

    lab07_1

---
+++ src/redux/reducer.js
@@ -0,0 +1,14 @@
+import {PROMOTIONS} from '../shared/promotions';
+import {LEADERS} from '../shared/leaders';
+import {COMMENTS} from '../shared/comments';
+import{DISHES} from "../shared/dishes";
+
+export const initialState={
+    dishes:DISHES,
+    comments:COMMENTS,
+    leaders:LEADERS,
+    promotions:PROMOTIONS
+}
+export const Reducer=(state=initialState,action)=>{
+    return state;
+}
